f25db6a2d06b7d133685910f919ba2349ef96072
JohnTWD <92451399+JohnTWD@users.noreply.github.com>
2024-01-25 11:08:22 +0800

    fix mixin clientworld, scoreboard

--- src/main/java/thunder/hack/injection/MixinScoreBoard.java
+++ src/main/java/thunder/hack/injection/MixinScoreBoard.java
@@ -5,25 +5,23 @@ import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.Team;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Scoreboard.class)
 public abstract class MixinScoreBoard {
-    @Final
-    @Shadow
+    @Final @Unique
     private Object2ObjectMap<String, Team> teamsByScoreHolder;
 
-    @Inject(method = "removeScoreHolderFromTeam", at = @At("HEAD"), cancellable = true)
-    public void removeScoreHolderFromTeamHook(String scoreHolderName, Team team, CallbackInfo ci) {
+    @Inject(method = "removePlayerFromTeam", at = @At("HEAD"), cancellable = true)
+    public void removeScoreHolderFromTeamHook(String playerName, Team team, CallbackInfo ci) {
         ci.cancel();
-        if (teamsByScoreHolder.get(scoreHolderName) != team) {
-            //("Player is either on another team or not on any team. Cannot remove from team '" + team.getName() + "'.");
+        if (teamsByScoreHolder.get(playerName) != team) {
             return;
         }
-        teamsByScoreHolder.remove(scoreHolderName);
-        team.getPlayerList().remove(scoreHolderName);
+        teamsByScoreHolder.remove(playerName);
+        team.getPlayerList().remove(playerName);
     }
 }
